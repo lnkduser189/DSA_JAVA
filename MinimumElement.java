b29147d67a3a5d6e132873fd47b24376b35b92cd
Vaishnavi Rushikesh Matade <vaishnavi@Vaishnavis-MacBook-Air.local>
2026-08-19 19:41:01 +0530

    Add find maximum element solution

---
+++ MinimumElement.java
@@ -0,0 +1,13 @@
+public class MinimumElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Minimum element in the array is: " + min);
+    }
+    
+}
