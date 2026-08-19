5e961267df709b2d96e67380e21ffdc08307e0d2
Vaishnavi Rushikesh Matade <vaishnavi@Vaishnavis-MacBook-Air.local>
2026-08-19 19:38:28 +0530

    Add find maximum element solution

---
+++ MaximumElement.java
@@ -0,0 +1,12 @@
+public class MaximumElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Maximum element in the array is: " + max);
+    }
+}
